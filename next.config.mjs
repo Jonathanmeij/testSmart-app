1bba6d766e9119e809e855b5b81f21d5a141bfe8
Jonathan Meij <jonathanvanmeij@gmail.com>
2023-03-10 22:00:10 +0100

    added source

--- next.config.mjs
+++ next.config.mjs
@@ -20,5 +20,14 @@ const config = {
     locales: ["en"],
     defaultLocale: "en",
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "source.unsplash.com",
+      },
+    ],
+  },
 };
+
 export default config;

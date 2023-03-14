3f189b622ba67d58780b14c707e2af1f481ab4ff
Jonathan Meij <jonathanvanmeij@gmail.com>
2023-03-14 21:04:25 +0100

    added getOne Route

--- src/server/api/routers/test.ts
+++ src/server/api/routers/test.ts
@@ -6,4 +6,20 @@ export const testRouter = createTRPCRouter({
   getAll: publicProcedure.query(({ ctx }) => {
     return ctx.prisma.test.findMany();
   }),
+  getOne: publicProcedure
+    .input(z.object({ id: z.string() }))
+    .query(({ ctx, input }) => {
+      return ctx.prisma.test.findUnique({
+        where: {
+          id: input.id,
+        },
+        include: {
+          questions: {
+            include: {
+              answers: true,
+            },
+          },
+        },
+      });
+    }),
 });

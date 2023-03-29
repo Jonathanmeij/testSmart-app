5b1701f2db294a4539d2d1d5dff2f17ba02e5177
Jonathan Meij <jonathanvanmeij@gmail.com>
2023-03-29 13:38:49 +0200

    fixed ui components index

--- components/ui/index.ts
+++ components/ui/index.ts
@@ -1,5 +1,24 @@
 import Navbar from "./Navbar";
 import Divider from "./Divider";
 import LinkButton from "./LinkButton";
+import Box from "./Box";
+import { Card } from "./Card";
+import Button from "./Button";
+import { Input, ListBox, TextArea } from "./Input";
+import { Modal, ModalBody, ModalFooter, ModalHeader } from "./Modal";
 
-export { Navbar, Divider, LinkButton };
+export {
+  Navbar,
+  Divider,
+  LinkButton,
+  Box,
+  Card,
+  Button,
+  Input,
+  ListBox,
+  TextArea,
+  Modal,
+  ModalBody,
+  ModalFooter,
+  ModalHeader,
+};
